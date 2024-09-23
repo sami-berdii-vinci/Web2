b1b3291ff524ba540ae51e7a67f93ff847d5a56d
S-Berdii <sami.berdii@student.vinci.be>
2024-09-23 14:07:16 +0200

    new: ex1.1

---
+++ tutorials/back/basic/exercises/1.1/basic-ts-api-boilerplate/routes/movies.ts
@@ -0,0 +1,32 @@
+import { Router } from "express";
+import { Movie } from "../types"
+
+const router = Router();
+
+const movies: Movie[] = [
+    {
+        id : 1,
+        title : "Les Schtroumpfs",
+        director : "Peyo",
+        duration : 93
+    },
+    {
+        id : 2,
+        title : "Le diner de cons",
+        director : "Louis de Funès",
+        duration : 102
+    },
+    {
+        id : 3,
+        title : "Avengers : Endgame",
+        director : "Stark Industries",
+        duration : 117
+    }
+
+]
+
+router.get("/", (_req, res) => {
+    return res.json(movies);
+  });
+
+export default router;
