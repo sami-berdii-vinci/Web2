4ea2c98f91f156e56375ce8a57025ca9b9f7313a
S-Berdii <sami.berdii@student.vinci.be>
2024-10-04 09:44:50 +0200

    followed tutorial (5) + created exercise 1.7 file

---
+++ exercises/1.7/routes/movies.ts
@@ -0,0 +1,129 @@
+import { Router } from "express";
+import { Movie, NewMovie } from "../types";
+
+const router = Router();
+
+const movies: Movie[] = [
+  {
+    id: 1,
+    title: "Les Schtroumpfs",
+    director: "Peyo",
+    duration: 93,
+  },
+  {
+    id: 2,
+    title: "L'avare",
+    director: "Louis de Funès",
+    duration: 102,
+  },
+  {
+    id: 3,
+    title: "Avengers : Endgame",
+    director: "Marvel",
+    duration: 117,
+  },
+];
+
+router.get("/", (req, res) => {
+  if (!req.query["duration-max"]) {
+    return res.json(movies);
+  }
+  const durationMax = Number(req.query["duration-max"]);
+  const filteredMovies = movies.filter((movie) => {
+    return movie.duration <= durationMax;
+  });
+  return res.json(filteredMovies);
+});
+
+router.get("/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const movie = movies.find((movie) => movie.id === id);
+  if (!movie) {
+    return res.sendStatus(404);
+  }
+  return res.json(movie);
+});
+
+router.post("/", (req, res) => {
+  const body: unknown = req.body;
+  if (
+    !body ||
+    typeof body !== "object" ||
+    !("title" in body) ||
+    !("director" in body) ||
+    !("duration" in body) ||
+    typeof body.title !== "string" ||
+    typeof body.director !== "string" ||
+    typeof body.duration !== "number" ||
+    body.duration <= 0 ||
+    !body.title.trim() ||
+    !body.director.trim()
+  ) {
+    return res.sendStatus(400);
+  }
+
+  for (const movie of movies) {
+    if (body.title === movie.title && body.director === movie.director){
+      return res.sendStatus(409);
+    }
+  }
+
+  const { title, director, duration } = body as NewMovie;
+
+  const nextId = movies.reduce((maxId, movie) => (movie.id > maxId ? movie.id : maxId), 0) + 1;
+  const newMovie: Movie = {
+    id: nextId,
+    title,
+    director,
+    duration
+  };
+
+  movies.push(newMovie);
+  return res.json(newMovie);
+});
+
+router.delete("/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const index = movies.findIndex((movie) => movie.id === id);
+  if (index === -1) {
+    return res.sendStatus(404);
+  }
+  const deletedElements = movies.splice(index, 1);
+  return res.json(deletedElements[0]);
+});
+
+router.patch("/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const movie = movies.find((movie) => movie.id === id);
+  if (!movie) {
+    return res.sendStatus(404);
+  }
+
+  const body: unknown = req.body;
+
+  if (
+    !body ||
+    typeof body !== "object" ||
+    ("title" in body && (typeof body.title !== "string" || !body.title.trim())) ||
+    ("director" in body && (typeof body.director !== "string" || !body.director.trim())) ||
+    ("duration" in body && (typeof body.duration !== "number"))
+  ) {
+    return res.sendStatus(400);
+  }
+
+  const { title, director, duration }: Partial<NewMovie> = body;
+
+  if (title) {
+    movie.title = title;
+  }
+  if (director) {
+    movie.director = director;
+  }
+  if (duration) {
+    movie.duration = duration;
+  }
+
+  return res.json(movie);
+});
+
+export default router;
--- tutorials/back/api/persistence/routes/drinks.ts
+++ tutorials/back/api/persistence/routes/drinks.ts
@@ -1,7 +1,10 @@
 import { Router } from "express";
+import path from "node:path";
 import { Drink, NewDrink } from "../types";
+import { parse, serialize } from "../utils/json";
+const jsonDbPath = path.join(__dirname, "/../data/drinks.json");
 
-const drinks: Drink[] = [
+const defaultDrinks: Drink[] = [
   {
     id: 1,
     title: "Coca-Cola",
@@ -47,6 +50,7 @@ const drinks: Drink[] = [
 const router = Router();
 
 router.get("/", (req, res) => {
+  const drinks = parse(jsonDbPath, defaultDrinks);
   if (!req.query["budget-max"]) {
     // Cannot call req.query.budget-max as "-" is an operator
     return res.json(drinks);
@@ -60,6 +64,7 @@ router.get("/", (req, res) => {
 
 router.get("/:id", (req, res) => {
   const id = Number(req.params.id);
+  const drinks = parse(jsonDbPath, defaultDrinks);
   const drink = drinks.find((drink) => drink.id === id);
   if (!drink) {
     return res.sendStatus(404);
@@ -88,8 +93,9 @@ router.post("/", (req, res) => {
     return res.sendStatus(400);
   }
 
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-  const { title, image, volume, price, } = body as NewDrink;
+  const { title, image, volume, price } = body as NewDrink;
+
+  const drinks = parse(jsonDbPath, defaultDrinks);
 
   const nextId =
     drinks.reduce((maxId, drink) => (drink.id > maxId ? drink.id : maxId), 0) +
@@ -104,21 +110,25 @@ router.post("/", (req, res) => {
   };
 
   drinks.push(newDrink);
+  serialize(jsonDbPath, drinks);
   return res.json(newDrink);
 });
 
 router.delete("/:id", (req, res) => {
   const id = Number(req.params.id);
+  const drinks = parse(jsonDbPath, defaultDrinks);
   const index = drinks.findIndex((drink) => drink.id === id);
   if (index === -1) {
     return res.sendStatus(404);
   }
   const deletedElements = drinks.splice(index, 1); // splice() returns an array of the deleted elements
+  serialize(jsonDbPath, drinks);
   return res.json(deletedElements[0]);
 });
 
 router.patch("/:id", (req, res) => {
   const id = Number(req.params.id);
+  const drinks = parse(jsonDbPath, defaultDrinks);
   const drink = drinks.find((drink) => drink.id === id);
   if (!drink) {
     return res.sendStatus(404);
@@ -155,8 +165,9 @@ router.patch("/:id", (req, res) => {
     drink.price = price;
   }
 
+  serialize(jsonDbPath, drinks);
+
   return res.json(drink);
 });
-
 
 export default router;
