be22edc985c1c3d5a489bad55e4b8932931a0dd2
S-Berdii <sami.berdii@student.vinci.be>
2024-10-07 14:04:50 +0200

    created services folder

---
+++ tutorials/back/api/services/app.ts
@@ -0,0 +1,24 @@
+import express, { ErrorRequestHandler } from "express";
+
+import usersRouter from "./routes/users";
+import pizzaRouter from "./routes/pizzas";
+import drinkRouter from "./routes/drinks";
+
+const app = express();
+
+const errorHandler: ErrorRequestHandler = (err, _req, res, _next) => {
+    console.error(err.stack);
+    return res.status(500).send("Something broke!");
+  };
+  
+app.use(errorHandler);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/users", usersRouter);
+app.use("/pizzas", pizzaRouter);
+app.use("/drinks", drinkRouter);
+app.use(express.json());
+
+export default app;
