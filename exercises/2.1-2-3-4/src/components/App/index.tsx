1a85dfa7fc30ca1f202162857700edb74739058d
S-Berdii <sami.berdii@student.vinci.be>
2024-10-18 10:09:55 +0200

    new: ex 2.3

--- exercises/2.1-2-3-4/src/components/App/index.tsx
+++ exercises/2.1-2-3-4/src/components/App/index.tsx
@@ -1,36 +1,60 @@
 import Header from "../Header";
-import Cinema from "../Main";
+import Cinema from "../Cinema";
+import { Movie } from "../../types";
 
 const App = () => {
   const pageTitle = "Informations sur les films dans les cinémas";
 
   const cinema1Name = "UGC DeBrouckère";
-  const movie1 = {
+
+  const moviesCinema1: Movie[] = [
+  {
     title: "HAIKYU-THE DUMPSTER BATTLE",
     director: "Susumu Mitsunaka",
-  }
-  const movie2 = {
+  },
+  {
     title: "GOODBYE JULIA",
     director: "Mohamed Kordofani",
-  };
+  },
+  {
+    title: "INCEPTION",
+    director: "Christopher Nolan",
+  },
+  {
+    title: "PARASITE",
+    director: "Bong Joon-ho",
+  },
+];
 
-  const cinema2Name = "UGC Toison d'Or";
-  const movie3 = {
+const cinema2Name = "UGC Toison d'Or";
+
+const moviesCinema2: Movie[] = [
+  {
     title: "THE WATCHERS",
     director: "Ishana Night Shyamalan",
-  };
-  const movie4 = {
+  },
+  {
     title: "BAD BOYS: RIDE OR DIE",
     director: "Adil El Arbi, Bilall Fallah",
-  };
+  },
+  {
+    title: "TENET",
+    director: "Christopher Nolan",
+  },
+  {
+    title: "THE IRISHMAN",
+    director: "Martin Scorsese",
+  },
+]; 
 
+  
   return (
     <div>
-      <Header title={pageTitle}/>
+      <Header title={pageTitle} />
 
-      <Cinema name={cinema1Name} movie1={movie1} movie2={movie2} />
+      <Cinema name={cinema1Name} movies={moviesCinema1} />
 
-      <Cinema name={cinema2Name} movie1={movie3} movie2={movie4} />
+      <Cinema name={cinema2Name} movies={moviesCinema2} />
     </div>
   );
 };
