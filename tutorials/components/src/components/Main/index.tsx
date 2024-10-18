c6c6cfe6b694f125b9d026a6ea1b00ead79b376a
S-Berdii <sami.berdii@student.vinci.be>
2024-10-18 09:39:31 +0200

    followed tutorial 2.b (2)

--- tutorials/components/src/components/Main/index.tsx
+++ tutorials/components/src/components/Main/index.tsx
@@ -1,5 +1,6 @@
 import './Main.css'
 import sound from "../../assets/sounds/Infecticide-11-Pizza-Spinoza.mp3"
+import PizzaMenu from './PizzaMenu';
 
 const Main = () => {
     return (
@@ -13,8 +14,10 @@ const Main = () => {
           <source src={sound} type="audio/mpeg" />
           Your browser does not support the audio element.
         </audio>
+        <PizzaMenu />
       </main>
     );
-};
+  };
+  
 
 export default Main;
