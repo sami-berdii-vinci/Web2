428afc1e634967e6fc31d5b26e78153c91b449ca
S-Berdii <sami.berdii@student.vinci.be>
2024-11-17 22:51:25 +0100

    new: ex 2.10

--- exercises/2.10-11-12/src/main.tsx
+++ exercises/2.10-11-12/src/main.tsx
@@ -1,10 +1,36 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./components/App";
 import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import HomePage from "./components/pages/HomePage";
+import App from "./components/App";
+import CinemaPage from "./components/pages/CinemaPage";
+import MovieListPage from "./components/pages/MovieListPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "",
+        element: <HomePage />,
+      },
+      {
+        path: "cinemas",
+        element: <CinemaPage />,
+      },
+      {
+        path: "movie-list",
+        element: <MovieListPage />,
+      }
+    ],
+  }, 
+  
+]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/> 
   </React.StrictMode>
 );
